136f8a0be628b7c23e17eb03003da9acc1808fee
Tom Morton <thomas.morton@thesalegroup.co.uk>
2015-03-12 20:44:26 +0000

    first pass

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# sequential
-Simple Sequential ID generator
+# Sequential
+Sequential lets you create sequences of ID numbers, optionally formatted.
+
+## Install
+
+With composer:
+
+    require "errant/sequential": "1.0.*"
+
+## Examples
+
+    $sequencer = new \Sequential\Sequence('tcp://10.0.0.1:6379');
+    $sequencer->reset('test_sequence');
+    echo $sequencer->getNextID('test_sequence');
+
+You can also format IDs:
+
+    echo $sequencer->getNextIDFormatted('test_sequence', function($sequence) { return pad_left($sequence, 4, '0'); });
---
+++ src/Sequence.php
@@ -0,0 +1,54 @@
+<?php
+namespace Sequential;
+
+/**
+ * A Simple Sequence Generatore, using Redis
+ *
+ * @category   Sequential
+ * @package    Sequence
+ * @author     Tom Morton <tom@errant.me.uk>
+ * @copyright  2015 Tom Morton
+ * @license    MIT
+ */
+class Sequence {
+
+    protected $_client;
+    protected $_data;
+
+    /**
+     * Construct Object
+     *
+     * @author Tom Morton
+     * @param mixed $connection Array or String connection for Redis
+     */
+    public function __construct($connection) 
+    {
+        $this->_client = new new Predis\Client($connection)
+    }
+
+    /**
+     * Gets the Next ID in a Sequence
+     *
+     * @author Tom Morton
+     * @param string $sequence The Redis key to sequence
+     */
+    public function getNextID($sequence)
+    {
+        return $this->_client->incr($sequence);
+    }
+
+    /**
+     * Format the Next ID with a callback
+     * 
+     * @author Tom Morton
+     * @param string $sequence The Redis key to sequence
+     * @param callable $formatter Callback which accepts ID string as a parameter and returns a formatted string
+     */
+    public function getNextIDFormatted($sequence, $formatter)
+    {
+        if(!is_callable($formatter)) {
+            throw new \Exception('Sequential\Sequence excepted Callable as formatter');
+        }
+        return call_user_func($formatter, (string)$this->getNextID($sequence));
+    }
+}
